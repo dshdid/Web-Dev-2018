2efaa0cddfcb312f60a32c9dd3f769951598f482
DO-NOT-TOUCH-ME\Donac <dona.shdid97@gmail.com>
2018-10-23 15:20:27 +0200

    dom5

--- js/script.js
+++ js/script.js
@@ -4,9 +4,31 @@ function greet(){
 
 function init(){
     document.getElementById("first").src = "images/flower2.png";
+    /**********************************************************/
     let images = document.getElementById("container").getElementsByTagName("img");
-    images[1].src = "images/flower1.png";
+    images[1].src = "images/flower1.png";    
+    /**********************************************************/
+    document.getElementById("t3").innerHTML = 'Your are doing great!';
+    /**********************************************************/
+    let image = document.getElementById("new_element"), 
+    imagechild = document.createElement("img");
+    image.appendChild(imagechild).src = "images/flower2.png";
+    /**********************************************************/
+    let spans = document.getElementById("rainbow").getElementsByTagName("span");
+    let colors = ["red", "orange","yellow", "green", "blue", "purpule", "pink"];
+    
+    for (var i = spans.length - 1; i >= 0; i--) {
+	   spans[i].style.color = colors[i];
+    }   
+    
+
 }
+
+
+
+
+
+
 
 
     
