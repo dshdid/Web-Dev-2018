b5b01783641831df4d26183432a1f16ca372a8aa
DO-NOT-TOUCH-ME\Donac <dona.shdid97@gmail.com>
2018-10-23 11:06:58 +0200

    dom2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function greet(){
 function init(){
     document.getElementById("first").src = "images/flower2.png";
     let images = document.getElementById("container").getElementsByTagName("img");
-    images[1].src = "flower1.png";
+    images[1].src = "images/flower1.png";
 }
 
 
